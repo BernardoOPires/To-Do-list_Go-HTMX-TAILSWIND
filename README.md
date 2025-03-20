2793f807daad3654d434fa27a390ad74c3fff631
Bernardo Oliveira Pires <bernardopires@pifpaf.com.br>
2025-03-20 15:46:01 -0300

    ajustes front

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Projeto com o foco em aprender o uso de htmx, tailswind e reforçar o go(com ênfase em mutex)
+
+Tecnologias:
+
+html/htmx
+tailwind/css
+go
+js
---
+++ frontend/template/scripts/scripts.js
@@ -0,0 +1,7 @@
+function abrirPopup() {
+   document.getElementById('uploadModal').classList.remove('hidden');
+}
+
+function fecharPopup(){
+    document.getElementById('uploadModal').classList.add('hidden')
+}
