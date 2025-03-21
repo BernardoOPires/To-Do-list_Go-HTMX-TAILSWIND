44cf0c219422ccaf8073539b0b0ae904198efe8b
Bernardo Oliveira Pires <bernardopires@pifpaf.com.br>
2025-03-21 15:14:48 -0300

    popup de ulpoad do arquivo

--- frontend/template/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-function abrirPopup() {
-   document.getElementById('uploadModal').classList.remove('hidden');
-}
-
-function fecharPopup(){
-    document.getElementById('uploadModal').classList.add('hidden')
-}
